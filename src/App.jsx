06de83bcfa9fbc5aa71f1b6e04cd0f17cacb6f0e
Øystein Malt <me@oysmal.com>
2022-03-11 14:19:35 +0100

    Use react-router Link in nav menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { CMSParserPage } from "./pages/CMSParserPage";
 
 export default function App() {
@@ -16,20 +16,24 @@ export default function App() {
 
   return (
     <div>
-      <header className="w-full bg-yellow-300">
-        <nav className="max-w-[1200px] mx-auto flex flex-row justify-end px-4 py-3">
-          <ul className="flex flex-row">
-            {cmsData.map((page) => (
-              <li className="list-none ml-4 first:ml-0 underline uppercase text-lg">
-                <a href={page.Url}>{page.Sheet}</a>
-              </li>
-            ))}
-          </ul>
-        </nav>
-      </header>
       <Router>
+        <header className="w-full bg-yellow-300">
+          <div className="max-w-[1200px] mx-auto flex flex-row justify-end px-4 py-3">
+            <nav className="flex flex-row">
+              {cmsData?.map((page) => (
+                <Link
+                  key={`nav-${page.Url}`}
+                  className="list-none ml-4 first:ml-0 underline uppercase text-lg"
+                  to={page.Url}
+                >
+                  {page.Sheet}
+                </Link>
+              ))}
+            </nav>
+          </div>
+        </header>
         <Routes>
-          {cmsData.map((page) => (
+          {cmsData?.map((page) => (
             <Route
               key={page.Url}
               path={page.Url}
